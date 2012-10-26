5f07d3f69fbf815158a3b7e77ecdc0b2d7dd79c2
Tony Ramirez <tony.ramirez@motionloft.com>
2012-10-26 16:55:34 -0700

    Install and config Devise.

--- config/application.rb
+++ config/application.rb
@@ -58,5 +58,7 @@ module Gagelist
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+    # From Devise
+    config.assets.initialize_on_precompile = false
   end
 end
