a3bf334bd69a0a1ba63c8e1faac8cd9cc224d22f
Tony Ramirez <tony.ramirez@motionloft.com>
2012-10-26 18:11:26 -0700

    Add devise views and User id to tasks

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121026235604) do
+ActiveRecord::Schema.define(:version => 20121027003153) do
 
   create_table "lists", :force => true do |t|
     t.string   "name"
@@ -26,7 +26,10 @@ ActiveRecord::Schema.define(:version => 20121026235604) do
     t.integer  "list_id"
     t.datetime "created_at",                     :null => false
     t.datetime "updated_at",                     :null => false
+    t.integer  "user_id"
   end
+
+  add_index "tasks", ["user_id"], :name => "index_tasks_on_user_id"
 
   create_table "users", :force => true do |t|
     t.string   "first_name"
