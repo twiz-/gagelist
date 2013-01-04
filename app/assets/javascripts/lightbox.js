9029bbf6640ab331504a0ac0402e008e20838012
Tony Ramirez <tony.michaelramirez@gmail.com>
2013-01-04 08:36:58 -0800

    Update copy and lightbox

--- app/assets/javascripts/lightbox.js
+++ app/assets/javascripts/lightbox.js
@@ -48,8 +48,8 @@ lightbox = new Lightbox options
   LightboxOptions = (function() {
 
     function LightboxOptions() {
-      this.fileLoadingImage = '../images/loading.gif';
-      this.fileCloseImage = '../images/close.png';
+      this.fileLoadingImage = '/app/assets/images/loading.gif';
+      this.fileCloseImage = '/app/assets/images/close.png';
       this.resizeDuration = 700;
       this.fadeDuration = 500;
       this.labelImage = "Image";
