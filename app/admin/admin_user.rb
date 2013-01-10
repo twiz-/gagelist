2e8195d433a51b00d83710f04d520d549266811b
Tony Ramirez <tony.michaelramirez@gmail.com>
2013-01-09 21:37:45 -0800

    Fix time for relevance

--- app/admin/admin_user.rb
+++ app/admin/admin_user.rb
@@ -2,10 +2,10 @@ ActiveAdmin.register AdminUser do
   index do                            
     column :email                     
     column :current_sign_in_at do |user|
-      Time.now.in_time_zone("Pacific Time (US & Canada)")
+      user.current_sign_in_at.in_time_zone("Pacific Time (US & Canada)")
     end        
     column :last_sign_in_at do |user|
-      Time.now.in_time_zone("Pacific Time (US & Canada)")
+      user.last_sign_in_at.in_time_zone("Pacific Time (US & Canada)")
     end           
     column :sign_in_count             
     default_actions                   
