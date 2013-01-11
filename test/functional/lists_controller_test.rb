06cb271a97ece553ba107598c45312c73f898636
Tony Ramirez <tony.michaelramirez@gmail.com>
2013-01-10 21:38:31 -0800

    Add before filters for access and update to 3.2.11

--- test/functional/lists_controller_test.rb
+++ test/functional/lists_controller_test.rb
@@ -1,7 +1,16 @@
 require 'test_helper'
 
 class ListsControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  setup do
+    @list = lists(:one)
+  end
+  
+  
+  test "User must be logged in to create a list" do
+    get :index
+    assert_response :redirect
+    assert_redirected_to new_user_session_path
+  end  
 end
+
