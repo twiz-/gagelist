aeac0e2109d21e777be9b28238c586fe7f887eda
Tony Ramirez <tony.michaelramirez@gmail.com>
2013-01-19 16:10:06 -0800

    change production email config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,7 +75,7 @@ Gagelist::Application.configure do
   config.action_mailer.default :charset => "utf-8"
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
-    port: "587",
+    port: 587,
     domain: "gmail.com",
     authentication: :plain,
     user_name: "nosensitiveinfo@gmail.com",
