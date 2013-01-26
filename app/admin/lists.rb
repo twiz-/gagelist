b3b5c80ef251bb1add74fc7138909bb3af6ab6ad
Tony Ramirez <tony.michaelramirez@gmail.com>
2013-01-26 15:29:08 -0800

    fix styling for show, style set pass page

---
+++ app/admin/lists.rb
@@ -0,0 +1,8 @@
+ActiveAdmin.register List do
+  index do 
+    column :name
+    column :user_id
+    column "Stared Using", :created_at 
+    column "Last Made a Change", :updated_at
+  end
+end
--- app/admin/users.rb
+++ app/admin/users.rb
@@ -1,5 +1,6 @@
 ActiveAdmin.register User do
   index do
+    column :id
     column :email
     column :first_name
     column :last_name
