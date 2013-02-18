9c2387cedd9f3c06e86f7849f76932f8c7f49664
Tony Ramirez <tony.michaelramirez@gmail.com>
2013-02-18 08:34:06 -0800

    Remove Devise invtie module

--- app/controllers/users/invitations_controller.rb
+++ app/controllers/users/invitations_controller.rb
@@ -1,16 +1,16 @@
-class Users::InvitationsController < Devise::InvitationsController
-  def update
-    logger = Logger.new('log/debug.log')
-    logger.info('--------Log for update invitation-------')
-    logger.info(params[:user][:invitation_token])
-    
-    user = User.find(:first, :conditions => ['invitation_token = ?', params[:user][:invitation_token]])
-    if !user.blank?
-      list_team_member = ListTeamMember.find(:first, :conditions => ['user_id = ? AND invitation_token = ?', user.id, user.invitation_token])
-      list_team_member.active = true
-      list_team_member.save
-      
-    end
-    super
-  end
-end
+# class Users::InvitationsController < Devise::InvitationsController
+#   def update
+#     logger = Logger.new('log/debug.log')
+#     logger.info('--------Log for update invitation-------')
+#     logger.info(params[:user][:invitation_token])
+#     
+#     user = User.find(:first, :conditions => ['invitation_token = ?', params[:user][:invitation_token]])
+#     if !user.blank?
+#       list_team_member = ListTeamMember.find(:first, :conditions => ['user_id = ? AND invitation_token = ?', user.id, user.invitation_token])
+#       list_team_member.active = true
+#       list_team_member.save
+#       
+#     end
+#     super
+#   end
+# end
