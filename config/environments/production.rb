cb3d3ab244a424c7e436840b1bff522aeb81a896
Tony Ramirez <tony.michaelramirez@gmail.com>
2013-02-28 22:17:10 -0800

    add production mailer settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,14 +69,14 @@ Gagelist::Application.configure do
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
   
-  config.action_mailer.default_url_options = { :host => 'staging23-refreshrunner.herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => 'refreshrunner.com' }
   
   #Sending email on the production side
   config.action_mailer.smtp_settings = {
     :enable_starttls_auto => true,
     :address => "smtp.gmail.com",
     :port => 587,
-    :domain => "heroku.com",
+    :domain => "refreshrunner.com",
     :authentication => :plain,
     :user_name => "nosensitiveinfo@gmail.com",
     :password => "no1sensitiveinfo"
