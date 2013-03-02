503992fc3ccb00d6926ff6f58a0a99abea970fa6
Tony Ramirez <tony.michaelramirez@gmail.com>
2013-03-02 13:51:35 -0800

    add ssl redirect for all domains

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery     
+  before_filter :check_domain
+  def check_domain
+    if Rails.env.production? and request.host.downcase != 'refreshrunner.com'
+      redirect_to request.protocol + 'refreshrunner.com' + request.fullpath, :status => 301
+    end
 end
