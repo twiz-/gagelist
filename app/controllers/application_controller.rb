37d92f59a349d3ce1395186625e3e1e53c73bafb
Tony Ramirez <tony.michaelramirez@gmail.com>
2013-03-02 12:49:34 -0800

    remove request hack and add secure gravatar compalint

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,3 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery  
-  before_filter :check_uri
-
-  def check_uri
-    if /^www/.match(request.host)
-        redirect_to request.protocol + request.host_with_port[4..-1] + request.request_uri 
-      end
-  end
-   
+  protect_from_forgery     
 end
