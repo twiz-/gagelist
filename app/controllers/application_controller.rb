0e11af76becf7b0a063b24b8a4c15a303f728290
Tony Ramirez <tony.michaelramirez@gmail.com>
2013-03-18 20:50:50 -0700

    remove whitespace and comments in domain check

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery     
-  # before_filter :check_domain
+  before_filter :check_domain
   before_filter :authenticate_user!
   before_filter :trail_ended?
   
   def check_domain
-      if Rails.env.production? and request.host.downcase != 'refreshrunner.com'
-        redirect_to request.protocol + 'refreshrunner.com' + request.fullpath, :status => 301
-      end
+    if Rails.env.production? and request.host.downcase != 'refreshrunner.com'
+      redirect_to request.protocol + 'refreshrunner.com' + request.fullpath, :status => 301
     end
+  end
   
   def trail_ended?
     if !current_user.blank? && !current_user.paid_user? && !current_user.invited_user?
