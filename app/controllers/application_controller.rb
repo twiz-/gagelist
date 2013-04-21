69636628fb569940b533334b909568ed7100ed16
Tony Ramirez <tony.michaelramirez@gmail.com>
2013-04-21 11:33:35 -0700

    add back domain check with updated wilcard cert

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,15 +2,15 @@ class ApplicationController < ActionController::Base
   include PublicActivity::StoreController
   
   protect_from_forgery     
-  # before_filter :check_domain
+  before_filter :check_domain
   before_filter :authenticate_user!
   before_filter :trail_ended?
   
-  # def check_domain
-  #     if Rails.env.production? and request.host.downcase != 'refreshrunner.com'
-  #       redirect_to request.protocol + 'refreshrunner.com' + request.fullpath, :status => 301
-  #     end
-  #   end
+  def check_domain
+    if Rails.env.production? and request.host.downcase != 'refreshrunner.com'
+      redirect_to request.protocol + 'refreshrunner.com' + request.fullpath, :status => 301
+    end
+  end
   
   def trail_ended?
     if !current_user.blank? && !current_user.paid_user? && !current_user.invited_user?
